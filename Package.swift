69bea903b8a56d47fa0d1e76a3803f6f176f3ff2
Cameron Pulsford <cpulsfor@me.com>
2021-10-31 12:33:39 -0400

    Tweak Package.swift.

--- Package.swift
+++ Package.swift
@@ -1,28 +1,25 @@
 // swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Layout",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Layout",
-            targets: ["Layout"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            targets: ["Layout"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Layout",
-            dependencies: []),
+            name: "Layout"
+        ),
         .testTarget(
             name: "LayoutTests",
-            dependencies: ["Layout"]),
+            dependencies: ["Layout"]
+        ),
     ]
 )
